c8d704b129ce32e2624ad7a57a6acd870f632147
Nithish <nithishbommireddy06@gmail.com>
2023-10-06 13:11:28 +0530

    chore(read-config): change path to correctly point config.yml

--- src/util/read-config.js
+++ src/util/read-config.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const configData = yaml.load(
-	fs.readFileSync(path.resolve(__dirname, "../config.yml"), "utf8")
+	fs.readFileSync(path.resolve(__dirname, "../../config.yml"), "utf8")
 );
 
 module.exports = configData;
